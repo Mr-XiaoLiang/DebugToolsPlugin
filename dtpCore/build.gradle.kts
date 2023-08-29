a5d5a229a5aea21a0f396ae78fa455ba44965845
lollipop <liujian594@hotmail.com>
2023-08-29 22:54:23 +0800

    feat: 改改Gradle

--- dtpCore/build.gradle.kts
+++ dtpCore/build.gradle.kts
@@ -29,11 +29,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        aidl = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
 }
